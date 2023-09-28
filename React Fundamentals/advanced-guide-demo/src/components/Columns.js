c5916f7593c4c2d4077d700c8ca63ec95b0991bb
mahibrill <135819680+mahibrill@users.noreply.github.com>
2023-09-28 10:10:58 +0530

    added advanced files

---
+++ React Fundamentals/advanced-guide-demo/src/components/Columns.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Columns() {
+    return (
+        <React.Fragment>
+            <td>Name</td>
+            <td>Brillio</td>
+        </React.Fragment>
+    );
+}
+
+export default Columns;
